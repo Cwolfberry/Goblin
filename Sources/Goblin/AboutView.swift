2d79b0f85420522d5fb8c513e40caccb8dfb9755
Cwolfberry <ysbzc1009@gmail.com>
2024-01-26 10:39:52 +0800

    init repo.

---
+++ Sources/Goblin/AboutView.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by avalan.zhang on 2024/1/26.
+//
+
+import SwiftUI
+
+public struct AboutView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        VStack(alignment: .center, spacing: 40) {
+            Text("this is goblin kit...")
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.red)
+            
+            Button("Enter") {
+                print("GoblinKit: enter btn is clicked...")
+            }
+            
+            Text("footnote...")
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
